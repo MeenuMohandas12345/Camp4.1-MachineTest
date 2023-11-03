ab24dcebae0a1cff88c82c2928e7835d55f48b40
MeenuMohandas12345 <149049123+MeenuMohandas12345@users.noreply.github.com>
2023-11-03 14:31:43 +0530

    Add files via upload

---
+++ Camp4MachineTest/Library/Book.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+
+    // Class representing a Book
+    public class Book
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public string Category { get; }
+
+        public Book(int id, string name, string category)
+        {
+            Id = id;
+            Name = name;
+            Category = category;
+        }
+
+        public string GetBookDetails()
+        {
+            return $"ID: {Id}  \nName: {Name} \nCategory: {Category}";
+        }
+    }
+
+    // Interface for managing books
+    public interface IBooks
+    {
+        void AddBook(int id, string name, string category);
+        void GetDetails();
+    }
+
+    // Class implementing the IBooks interface
+    public class Books : IBooks
+    {
+        private List<Book> bookList = new List<Book>();
+
+        // Method to add book details
+        public void AddBook(int id, string name, string category)
+        {
+
+
+            try
+            {
+                if (bookList.Exists(book => book.Id == id))
+                {
+                    Console.WriteLine("Book with the same ID already exists.");
+                }
+                else
+                {
+                    Book newBook = new Book(id, name, category);
+                    bookList.Add(newBook);
+                    Console.WriteLine("Book added successfully!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+            
+        }
+
+        // Method to retrieve book details
+        public void GetDetails()
+        {
+            if (bookList.Count == 0)
+            {
+                Console.WriteLine("No books available.");
+                return;
+            }
+
+            Console.WriteLine("Book Details");
+            Console.WriteLine("--------------");
+            foreach (var book in bookList)
+            {
+                Console.WriteLine(book.GetBookDetails());
+            }
+        }
+
+        // Method to search for a book by ID
+        public void SearchBookById(int id)
+        {
+            try
+            {
+                var foundBook = bookList.Find(book => book.Id == id);
+                if (foundBook != null)
+                {
+                    Console.WriteLine($"Book found with ID {id}:");
+                    Console.WriteLine($"ID: {foundBook.Id}, Name: {foundBook.Name}, Category: {foundBook.Category}");
+                }
+                else
+                {
+                    Console.WriteLine($"No book found with ID {id}.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+            
+        }
+    }
+
+
+
+}
+
+
+
---
+++ Camp4MachineTest/Library/Program.cs
@@ -0,0 +1,148 @@
+﻿/*SET -1
+
+Create an application to manage a library.The application will 
+have an interface Ibooks that has the methods AddBooks and GetDetails
+A class Books that has the methods AddBooks()to add the details
+of the book like id, name and category  and GetBookDetails() to
+retrieve the details of the books. This class should implement
+from IBooks .The AddBooks and GetBookDetails should be a choice 
+get from the user.Retrieve the details of the books into a suitable 
+collection Handle all exceptions at runtime.*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+   
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Library Management System");
+            Books library = new Books();
+
+            while (true)
+            {
+                
+                Console.WriteLine("\n1. Add Book");
+                Console.WriteLine("2. Get All Book Details");
+                Console.WriteLine("3. Search Book by ID");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine();
+
+                Console.Write("Enter your choice (1-4): ");
+                int choice;
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Write("Enter Book ID: ");
+                            int id = int.Parse(Console.ReadLine());
+
+                            Console.Write("Enter Book Name: ");
+                            string name = Console.ReadLine();
+
+                            Console.Write("Enter Book Category: ");
+                            string category = Console.ReadLine();
+
+                            library.AddBook(id, name, category);
+                            break;
+
+                        case 2:
+                            library.GetDetails();
+                            break;
+
+                        case 3:
+                            Console.Write("Enter Book ID to search: ");
+                            int searchId = int.Parse(Console.ReadLine());
+                            library.SearchBookById(searchId);
+                            Console.WriteLine();
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Exiting the program");
+                            return;
+
+                        default:
+                            Console.WriteLine("Invalid choice. Please enter a number between 1 and 4.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
